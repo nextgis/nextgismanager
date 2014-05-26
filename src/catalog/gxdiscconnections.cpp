4c85413e5a24b0ecb49a1bc9fdb882e6c9a05844
Dmitry Baryshnikov <bishop.dev@gmail.com>
2014-05-27 00:48:35 +0400

    check available of disk connection

--- src/catalog/gxdiscconnections.cpp
+++ src/catalog/gxdiscconnections.cpp
@@ -3,7 +3,7 @@
  * Purpose:  wxGxDiscConnections class.
  * Author:   Dmitry Baryshnikov (aka Bishop), polimax@mail.ru
  ******************************************************************************
-*   Copyright (C) 2010-2013 Bishop
+*   Copyright (C) 2010-2013 Dmitry Baryshnikov
 *
 *    This program is free software: you can redistribute it and/or modify
 *    it under the terms of the GNU General Public License as published by
