67866139f5c2dd0c9121af77435bc70c45f9a3de
Dmitry Baryshnikov <bishop.dev@gmail.com>
2015-12-08 18:38:41 +0300

    fix geometry type in import dialog

--- src/catalogui/createremotedlgs.cpp
+++ src/catalogui/createremotedlgs.cpp
@@ -915,8 +915,12 @@ wxGISVectorImportPanel::wxGISVectorImportPanel(wxGISFeatureDataset *pSrcDs, wxGx
 
     wxStaticText *pInputStaticTextAdd = new wxStaticText(this, wxID_ANY, wxEmptyString, wxDefaultPosition, wxDefaultSize, 0);
     fgSizer1->Add(pInputStaticTextAdd, 0, wxALL | wxALIGN_CENTER_VERTICAL | wxALIGN_RIGHT, 5);
+    
+    OGRwkbGeometryType eGeomType = eFilterGeomType;
+    if(eGeomType == wkbUnknown)
+        eGeomType = pSrcDs->GetGeometryType();
 
-    wxString sAdds = wxString::Format(wxT("%s (%d)"), wxGetTranslation(OGRGeometryTypeToName(eFilterGeomType)).c_str(), nFeatureCount);
+    wxString sAdds = wxString::Format(wxT("%s (%d)"), wxGetTranslation(OGRGeometryTypeToName(eGeomType)).c_str(), nFeatureCount);
     wxStaticText *pInputStaticTextAddVal = new wxStaticText(this, wxID_ANY, sAdds, wxDefaultPosition, wxDefaultSize, wxST_ELLIPSIZE_MIDDLE);
     fgSizer1->Add(pInputStaticTextAddVal, 1, wxALL | wxEXPAND | wxALIGN_CENTER_VERTICAL, 5);
 	
